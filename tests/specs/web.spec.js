400007192175a1755756e4bdb1933480211f4c07
asvynarov <asvynarov@amixy.com>
2022-04-06 19:59:40 +0300

    [hotfix] remover compiled files

--- tests/specs/web.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Shoult start chrome', async () => {
-    it('web app loaded', async () => {
-        await browser.url('https://test-org.qa-auto.taskmaverick-feature.com/');
-        await browser.pause(3000);
-    });
-});
-//# sourceMappingURL=web.spec.js.map
